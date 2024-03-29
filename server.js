f67c686c014ddbefb659399f6182dc0c7e9cf2ff
ShirakuGIT <shiraku.official@gmail.com>
2024-03-29 16:41:25 +0530

    middleware, config, models.

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const cors = require("cors");
 
 const app = express();
 
+const db = require("./app/models");
+const Role = db.role;
+
+db.sequelize.sync({ force: true }).then(() => {
+    console.log('Drop and Resync Db');
+    initial();
+});
+
 var corsOptions = {
     origin: "http://localhost:8080"
 };
@@ -21,3 +29,20 @@ const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 });
+
+function initial() {
+    Role.create({
+        id: 1,
+        name: "user"
+    });
+
+    Role.create({
+        id: 2,
+        name: "moderator"
+    });
+
+    Role.create({
+        id: 3,
+        name: "admin"
+    });
+}
